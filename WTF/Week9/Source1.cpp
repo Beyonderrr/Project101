0c9c97e938f7137822115f9556c1c972f13e519d
Saharat Wetsansern <canicall.mine7@gmail.com>
2021-12-14 22:44:38 +0700

    Week9

---
+++ WTF/Week9/Source1.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int x, i = 2, y = 0;
+    printf("Enter a number : ");
+    scanf("%d", &x);
+    while (i < x)
+    {
+        if (x % i == 0)
+        {
+            y += i;
+        }
+        i++;
+    }
+    if (y > 0)
+    {
+        printf("%d is not a prime number", x);
+    }
+    else
+    {
+        printf("%d is a prime number", x);
+    }
+
+    return 0;
+}
